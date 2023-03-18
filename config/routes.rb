0cfa277e51fbb89ac538e66faf02a000be2c77cd
xieheng <xieheng0915@gmail.com>
2023-03-18 14:12:13 +0900

    Refactor code

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
