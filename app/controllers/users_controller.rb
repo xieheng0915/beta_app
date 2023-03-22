278f69f4bea4caa6131b293ab0592a3b73f3363e
xieheng <xieheng0915@gmail.com>
2023-03-22 20:03:04 +0900

    added sign up page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    #byebug
+    @user = User.new(user_params)
+    if  @user.save
+      flash[:notice] = "Welcome to Beta blog, #{@user.username}, You have signed up successfully."
+      redirect_to articles_path  
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update]
   resources :articles
+  #resources :users, only: [:new, :create]
+  get 'signup', to: 'users#new'
+  #post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  
 end
